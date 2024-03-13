09ff688c0bf5a634028fd897edecb91630903eb2
Yerzhan Rsaliyev <rsaliyev03@gmail.com>
2024-03-13 23:15:57 +0500

    search function

--- Lab6/src/app/albums/albums.component.ts
+++ Lab6/src/app/albums/albums.component.ts
@@ -3,13 +3,15 @@ import {AlbumsService} from "../albums.service";
 import {Album} from "../album/album.module";
 import {RouterLink} from "@angular/router";
 import {CommonModule} from "@angular/common";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-albums',
   standalone: true,
   imports: [
     RouterLink,
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './albums.component.html',
   styleUrl: './albums.component.css'
@@ -17,21 +19,33 @@ import {CommonModule} from "@angular/common";
 export class AlbumsComponent implements OnInit{
 
   albums!: Album[];
+  filteredAlbums!: Album[];
   loaded: boolean = false;
+  filterText: string = '';
 
-  constructor(private albumService: AlbumsService) {
-  }
+  constructor(private albumService: AlbumsService) {}
+
   ngOnInit(): void {
     this.albumService.getAlbums().subscribe((albums) => {
       this.albums = albums;
+      this.filteredAlbums = albums;
       this.loaded = true;
-    })
+    });
+  }
+
+  filterResults() {
+    const id = parseInt(this.filterText);
+    if (!isNaN(id)) {
+      this.filteredAlbums = this.albums.filter(album => album.id === id);
+    } else {
+      this.filteredAlbums = this.albums;
+    }
   }
 
   deletePost(id: number) {
-    this.albums = this.albums.filter(a => a.id !== id)
+    this.filteredAlbums = this.filteredAlbums.filter(a => a.id !== id);
     this.albumService.deleteAlbum(id).subscribe(() => {
       console.log("Album deleted successfully!")
-    })
+    });
   }
 }
